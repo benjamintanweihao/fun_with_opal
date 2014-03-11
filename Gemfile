63773c54fc4692f22d0e5602c6527a60121a4783
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-03-12 00:25:37 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.17'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'haml-rails'
+gem 'opal-rails'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunWithOpal::Application.config.secret_token = '2662c706901c3c1ab042e8c7d2d9f8fbddda7a4c434cbfad055d677e29126fa4537aa2d9deddf23bdfb705e1792891b028a7635ad0a51884c58e92cae86d9cbc'
